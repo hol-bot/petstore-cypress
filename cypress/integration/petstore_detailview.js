c0cad683d189760861de693cf04275a7e739aa33
holger <holger.hansson@nortal.com>
2019-10-23 14:20:06 +0300

    Stringify json

--- cypress/integration/petstore_detailview.js
+++ cypress/integration/petstore_detailview.js
@@ -4,9 +4,9 @@ describe("On pet's detail view", function() {
 
   // TO-DO programatically set state
   it('should be possible to cancel editing', function() {
-    cy.request('POST', '/petstore/api/v1/pet/', {"name":timestamp,"petType":"Fish","birthDate":"2019-10-22T21:00:00.000Z"})
+    cy.request('POST', '/petstore/api/v1/pet/', {"name":  JSON.stringify(timestamp),"petType":"Fish","birthDate":"2019-10-22T21:00:00.000Z"})
       .then((response) => {
-        expect(response.body).to.have.property('name', timestamp)
+        expect(response.body).to.have.property("name", JSON.stringify(timestamp))
       })
   })
 
